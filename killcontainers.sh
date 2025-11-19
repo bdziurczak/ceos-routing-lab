c820c9d0cabdb0998d4bbfb7c8262c3e13b63c4f
bdziurczak <bbb05bart@gmail.com>
2025-11-19 11:55:27 +0100

    Simplify configuration, new deploy script

--- killcontainers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#when I redeploy labs there's often problem that clab destroy don't remove containers. this simple script automate killing containers
-# then it deploys lab
-docker stop $(docker ps -a -q)
-sleep 5
-docker kill $(docker ps -a -q)
-
-sudo clab deploy
