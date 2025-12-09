288be11482348d4940193c3c8ebf3cbfd5d89e0b
bdziurczak <bbb05bart@gmail.com>
2025-12-09 11:54:01 +0100

    configured DHCP. Client debian image update. Client debian instances wait for network devices to configure

--- configs/lan-pc1-setup.sh
+++ configs/lan-pc1-setup.sh
@@ -2,3 +2,6 @@
 echo "root:root" | chpasswd
 ip link set eth1 up
 dhclient -v eth1
+ip route add 112.112.116.0/24 via 192.168.150.1
+ip route add 112.112.114.0/24 via 192.168.150.1
+ip route add 192.168.150.128/25 via 192.168.150.1
